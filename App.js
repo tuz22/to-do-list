c6d1abffdeac2b41f8359405805b6de204d988b2
tuz22 <seungji.dev@gmail.com>
2023-03-19 06:02:59 +0900

    투두 체크 UI, KeyboardAvoidingView 추가

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import { useEffect } from 'react';
-import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Image, KeyboardAvoidingView, Pressable, Keyboard } from 'react-native';
 import dayjs from 'dayjs';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 import { Ionicons } from '@expo/vector-icons'; 
 
 import { runPracticeDayjs } from './src/practive-dayjs';
-import { getCalendarColumns, getDayText, getDayColor } from './src/util';
+import { getCalendarColumns, getDayText, getDayColor, statusBarHeight, ITEM_WIDTH, bottomSpace } from './src/util';
 import { useCalendar } from './src/hook/use-calendar';
 import { useTodoList } from './src/hook/use-todo-list';
 import Calendar from './src/Calendar';
 import Margin from './src/Margin';
-
-const statusBarHeight = getStatusBarHeight(true);
+import AddTodoInput from './src/AddTodoInput';
 
 export default function App() {
   const now = dayjs(); // 현재시각
@@ -29,6 +28,8 @@ export default function App() {
 
   const {
     todoList, 
+    input,
+    setInput,
   } = useTodoList(selectedDate)
   
   const columns = getCalendarColumns(selectedDate); // 현재시각을 기준으로 캘린더에 담긴 컬럼들을 가져옴
@@ -68,7 +69,7 @@ export default function App() {
       <View 
         style={{ 
           flexDirection: 'row',
-          width: 220, 
+          width: ITEM_WIDTH, 
           // backgroundColor: todo.id % 2 === 0 ? 'pink' : 'lightblue',
           alignSelf: 'center',
           paddingVertical: 10,
@@ -86,6 +87,10 @@ export default function App() {
     )
   }
 
+  const onPressAdd = () => {
+
+  }
+
   useEffect(() => {
     runPracticeDayjs();
 
@@ -95,9 +100,12 @@ export default function App() {
   // useEffect(() => {
   //   console.log('selectedDate값 변경:', dayjs(selectedDate). format('YYYY.MM.DD'));
   // }, [selectedDate])
-
+  
+/* Pressable = TouchableOpacity activeOpacity={1} */
   return (
-    <View style={styles.container}>
+    <Pressable 
+      style={styles.container} 
+      onPress={Keyboard.dismiss}> 
       <Image
         source={{
           uri: "https://img.freepik.com/free-photo/white-crumpled-paper-texture-for-background_1373-159.jpg?w=1060&t=st=1667524235~exp=1667524835~hmac=8a3d988d6c33a32017e280768e1aa4037b1ec8078c98fe21f0ea2ef361aebf2c",
@@ -110,13 +118,26 @@ export default function App() {
       />
       
 
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <>
+          <FlatList
+            data={todoList}
+            contentContainerStyle={{ paddingTop: statusBarHeight }}
+            ListHeaderComponent={ListHeaderComponent} 
+            renderItem={renderItem}
+          />
 
-      <FlatList
-        data={todoList}
-        contentContainerStyle={{ paddingTop: statusBarHeight }}
-        ListHeaderComponent={ListHeaderComponent} 
-        renderItem={renderItem}
-      />
+          <AddTodoInput 
+            value={input}
+            onChangeText={setInput}
+            placeholder={`${dayjs(selectedDate).format('M.D')}에 추가할 투두`}
+            onPressAdd={onPressAdd}
+          />
+        </>
+      </KeyboardAvoidingView>
+
+      <Margin height={bottomSpace} />
 
       <DateTimePickerModal
         isVisible={isDatePickerVisible}
@@ -124,7 +145,7 @@ export default function App() {
         onConfirm={handleConfirm}
         onCancel={hideDatePicker}
       />
-    </View>
+    </Pressable>
   );
 }
 
--- src/util.js
+++ src/util.js
@@ -1,5 +1,11 @@
 import dayjs from "dayjs";
 
+import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
+
+export const statusBarHeight = getStatusBarHeight(true);
+export const bottomSpace = getBottomSpace();
+export const ITEM_WIDTH = 220;
+
 /* 달력의 공백일 채우기 */
 export const fillEmptyColumns = (columns, start, end) => {
   const filledColumns = columns.slice(0);
